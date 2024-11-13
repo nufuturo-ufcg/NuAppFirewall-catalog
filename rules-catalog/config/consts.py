a63bb8bec009591d30f2952e85441e876fde7697
ViniciusUltraNX <115042218+ViniciusUltraAD@users.noreply.github.com>
2024-11-13 10:55:41 -0300

    Add constant of txt file path of block apps

--- rules-catalog/config/consts.py
+++ rules-catalog/config/consts.py
@@ -1,5 +1,6 @@
 # [Used only when rules are generated from Cortex XDR logs]
-NETWORK_ACCESS_LOGS_SV_PATH = '' # Provide the path to csv logs ->input<-
+NETWORK_ACCESS_LOGS_SV_PATH = '' # Provide the path to (t/c)sv logs ->input<-
+NETWORK_BLOCK_APP_NAMES_PATH = '' # Provide the path to txt file of name of apps to be blocked ->input<-
 NETWORK_ACCESS_LOGS_CSV_OUTPUT_PATH = 'data/paths.csv' # Provide the path to save processed logs ->output<-
 
 # General configuration
