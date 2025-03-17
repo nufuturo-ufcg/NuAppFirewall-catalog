779f4ed700657e60fc86134b545d31d1d9153894
ViniciusADelgado <vinicius.ataide.delgado@ccc.ufcg.edu.br>
2025-03-17 14:40:17 -0300

    Update consts.py and main file

--- rules-catalog/config/consts.py
+++ rules-catalog/config/consts.py
@@ -1,6 +1,7 @@
 # [Used only when rules are generated from EDR logs]
 NETWORK_ACCESS_LOGS_SV_PATH = '' # Provide the path to (t/c)sv logs ->input<-
-NETWORK_BLOCK_APP_NAMES_PATH = '' # Provide the path to txt file of name of apps to be blocked ->input<-
+NETWORK_BLOCK_APP_NAMES_PATH = '' # Provide the path to csv file of name of apps to be blocked ->input<-
+NETWORK_BLOCK_PORTS_PATH = '' # Provide the path to the txt file of ports to be blocked ->input<-
 NETWORK_ACCESS_LOGS_CSV_OUTPUT_PATH = 'data/paths.csv' # Provide the path to save processed logs ->output<-
 
 # General configuration
--- rules-catalog/main.py
+++ rules-catalog/main.py
@@ -32,6 +32,28 @@ def main():
         rules_dict = combine_allow_and_block_rules_dict(rules_dict, block_ports_dict)
 
         save_rules_file(rules_dict, consts.RULES_FILE_PATH, args.format)
+    elif args.block and args.port_blocking:
+        consts.NETWORK_BLOCK_APP_NAMES_PATH = args.block
+        consts.NETWORK_BLOCK_PORTS_PATH = args.port_blocking
+        consts.RULES_FILE_PATH = args.output
+
+        mod.process_block_file(consts.NETWORK_BLOCK_APP_NAMES_PATH, args.simplified, consts.NETWORK_ACCESS_LOGS_CSV_OUTPUT_PATH)
+        block_rules_dict = create_final_rules_dict(consts.NETWORK_ACCESS_LOGS_CSV_OUTPUT_PATH, False)
+
+        mod.process_block_ports(consts.NETWORK_BLOCK_PORTS_PATH, args.simplified, consts.NETWORK_ACCESS_LOGS_CSV_OUTPUT_PATH)
+        block_ports_dict = create_final_rules_dict(consts.NETWORK_ACCESS_LOGS_CSV_OUTPUT_PATH, False)
+
+        rules_dict = combine_allow_and_block_rules_dict(block_rules_dict, block_ports_dict)
+
+        save_rules_file(rules_dict, consts.RULES_FILE_PATH, args.format)
+    elif args.port_blocking:
+        consts.NETWORK_BLOCK_PORTS_PATH = args.port_blocking
+        consts.RULES_FILE_PATH = args.output
+
+        mod.process_block_ports(consts.NETWORK_BLOCK_PORTS_PATH, args.simplified, consts.NETWORK_ACCESS_LOGS_CSV_OUTPUT_PATH)
+        block_ports_dict = create_final_rules_dict(consts.NETWORK_ACCESS_LOGS_CSV_OUTPUT_PATH, False)
+
+        save_rules_file(block_ports_dict, consts.RULES_FILE_PATH, args.format)
     elif args.block and args.input:
         consts.NETWORK_ACCESS_LOGS_SV_PATH = args.input
         consts.NETWORK_BLOCK_APP_NAMES_PATH = args.block
